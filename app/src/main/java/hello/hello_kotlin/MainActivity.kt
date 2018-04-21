b9338adca0a0bb74f42f37b5eda5d8d6c97590c3
shhayash <shhayash@yahoo-corp.jp>
2018-04-22 00:10:20 +0900

    はろーことりん

--- app/src/main/java/hello/hello_kotlin/MainActivity.kt
+++ app/src/main/java/hello/hello_kotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package hello.hello_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val message: TextView = this.findViewById(R.id.text) as TextView
+        message.text = "ハロー ことりん"
     }
 }
